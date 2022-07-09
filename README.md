82069138630d3366b44c25b8be38826b7a979b0c
Aishik Mukherjee <aishikm2002@gmail.com>
2022-07-09 23:02:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Python viewbot
+
+## Description:
+* A viewbot script written in python
+* Made using <a href="https://www.selenium.dev/">Selenium</a>
+* Default browser set as <a href="https://www.microsoft.com/en-us/edge">Microsoft Edge</a> using <a href="https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/">Microsoft Edge WebDriver</a>. <a href="https://chrome.google.com/webstore/detail/free-vpn-for-chrome-vpn-p/majdfhpaihoncoakbjgbdhglocklcgno?hl=en">VeePN</a> extension is used.
+
+## Instructions:
+* Make sure <a href="https://www.python.org/">Python</a> is installed in the system
+* Clone the repository :<br>
+```bash 
+git clone https://github.com/AISHIK999/python_viewbot.git
+```
+* Move into the directory:<br>
+```bash 
+cd python_viewbot
+```
+* Give executable permissions to the edgedriver:<br>
+```bash
+chmod +x msedgedriver
+```
+* Make changes to the script:
+Open the <code>main.py</code> in a text editor.
+Replace the <b>my_channel</b> value assigned to the variable: <b>channel_name</b> and save it.
+<br>
+Now open the <code>bot.sh</code> file in a text editor and change the active instances and the sleep time as directed by the comments. Save and exit.<br>
+
+* Run the script:
+```bash
+bash bot.sh
+```
+
+# DISCLAIMER:
+This script was created only for educational purposes. I am in no way to be held responsible for any kind of damage dealt (public or personal) to someone for using this script. 
---
+++ main.py
@@ -0,0 +1,42 @@
+# Import necessary libraries
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Change the channel name below (keep the quotation marks)
+channel_name = "my_channel"
+
+# Set necessary variables
+site = f"https://www.twitch.tv/{channel_name}"
+path_to_webdriver = "./msedgedriver"
+vpn_extension = "./vpn.crx"
+vpn_html = "chrome-extension://majdfhpaihoncoakbjgbdhglocklcgno/html/foreground.html"
+
+# Edgedriver Options
+options = Options()
+options.add_extension(vpn_extension)
+driver_service = Service(executable_path=path_to_webdriver)
+driver = webdriver.Edge(service=driver_service, options=options)
+
+# Close the VPN installation window and load the VPN HTML
+while True:
+    if len(driver.window_handles) == 2:
+        break
+driver.switch_to.window(driver.window_handles[1])
+driver.close()
+driver.switch_to.window(driver.window_handles[0])
+driver.get(vpn_html)
+
+# Navigate through the menu and connect to VPN
+WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, '//button[@class="next"]')))
+for i in range(2):
+    driver.find_element(by = "xpath", value = '//button[@class="next"]').click()
+WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, '//div[@id="mainBtn"]')))
+driver.find_element(by="xpath", value='//div[@id="mainBtn"]').click()
+WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//div[@class="connected"]')))
+
+# Let the show begin
+driver.get(site)
