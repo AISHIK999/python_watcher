2696f648397a97499655ded233e02165ce2737ef
Aishik Mukherjee <aishikm2002@gmail.com>
2022-12-19 23:43:14 +0530

    Cleanup
    * Cleanup the driver code
    * Update the webdriver binary

--- bot.sh
+++ bot.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-startChromeBot(){
-    nestedFunction(){
-        python3 main.py
-        MY_PID=$!
-        sleep 60m           # Run each instance for 60 mins
-        kill -KILL $MY_PID
-    }
-    while true;do
-        nestedFunction
-    done
-}
-
 while true;do
     for i in {0..5};do      # Run 5 instances
-        startChromeBot &
+        python3 main.py &
     done
     wait
 done
